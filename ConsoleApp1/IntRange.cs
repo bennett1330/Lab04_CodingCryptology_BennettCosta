fa28c072ac2470806931a0a99a25dfcf8c38b5a6
Matt Bennett <mgbennett1330@gmail.com>
2017-09-30 14:58:37 -0400

    Added functionality to adjust number to correct range for substitution decryption

---
+++ ConsoleApp1/IntRange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class IntRange
+    {
+        static public int Min, Max;
+
+        Range( int min, int max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public int IterativeAdjust(int value, int adjustment)
+        {
+            bool under = value < Min;
+            bool over = value > Max;
+
+            if (!under && !over)
+                return value;
+            else if (under)
+                return IterativeAdjust(value - adjustment, adjustment);
+            else if (over)
+                return IterativeAdjust(value + adjustment, adjustment);
+            else
+                throw new System.Exception("ERROR: Congrats, you broke math!");
+        }
+
+
+    }
+}
