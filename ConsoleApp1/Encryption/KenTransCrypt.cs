4dffaa1b38162c9723d5c750c1e02865c02840b6
Matt Bennett <mgbennett1330@gmail.com>
2017-10-01 16:41:01 -0400

    KenTransCrypt working encrypt decrypt

--- ConsoleApp1/Encryption/KenTransCrypt.cs
+++ ConsoleApp1/Encryption/KenTransCrypt.cs
@@ -7,27 +7,34 @@ namespace ConsoleApp1
     public class KenTransCrypt : Encryption
     {
         
+        private char[] RotateCharArray(char[] chartext)
+        {
+            char[] shifted = new char[chartext.Length];
+            // % is c# modulo operator used to allow wrap around
+            for (int i = 0; i < chartext.Length; i++)
+                shifted.SetValue(chartext[(i + 1) % chartext.Length], i);
+            return shifted;
+        }
+
         protected override string Encrypt(string plaintext, int key)
         {
-            int wrapChar;
+            // Subtract 48 to adjust from ascii to correct int value
+            int iKey = key.ToString()[0] - 48;
             char[] chartext = plaintext.ToCharArray();
-            for (int a = 0; a < key.ToString().Length; a++)
-            {
-                wrapChar = plaintext[0];
-                for (int b = 0; b < chartext.Length; b++)
-                {
-                    if (b + 1 == chartext.Length)
-                        chartext.SetValue((char)wrapChar, b);
-                    else
-                        chartext.SetValue((char)plaintext[b + 1], b);
-                }
-            }
-            return chartext.ToString();
+            for (int a = 0; a < iKey; a++)
+                chartext = RotateCharArray(chartext);
+            return new string(chartext);
         }
 
         protected override string Decrypt(string ciphertext, int key)
         {
-            throw new NotImplementedException();
+            // Subtract 48 to adjust from ascii to correct int value
+            int iKey = key.ToString()[0] - 48;
+            char[] chartext = ciphertext.ToCharArray();
+            int shift = Math.Abs(ciphertext.Length - iKey);
+            for (int a = 0; a < shift; a++)
+                chartext = RotateCharArray(chartext);
+            return new string(chartext);
         }
 
     }
